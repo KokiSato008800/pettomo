0d742ddfeae1213260af4422f56b9100178162f0
KokiSato008800 <kouki00800@gmail.com>
2023-08-29 12:16:16 +0900

    firebase hostingでホスティングするところまで完了

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { initializeApp } from "firebase/app";
 
 Vue.config.productionTip = false
 
@@ -9,3 +10,16 @@ new Vue({
   render: h => h(App),
 }).$mount('#app')
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZzENoTzevAODSfilXR9kqyppDLiS3GMQ",
+  authDomain: "pettomo-ac486.firebaseapp.com",
+  projectId: "pettomo-ac486",
+  storageBucket: "pettomo-ac486.appspot.com",
+  messagingSenderId: "840900912549",
+  appId: "1:840900912549:web:147734251c7f8a3cb2744c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log(app);
